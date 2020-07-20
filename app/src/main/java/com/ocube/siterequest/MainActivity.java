bb36febf0ba8ecc9ebe1a48ed94ec65110f38c46
OpenCubeKenya <opencubetechnologies@gmail.com>
2020-07-20 17:56:08 +0300

    Initial commit

---
+++ app/src/main/java/com/ocube/siterequest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.ocube.siterequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Main Screen Buttons
+        Button siteRequest = findViewById(R.id.siteRequest);
+        Button dailyReport = findViewById(R.id.dailyReport);
+        Button salaryInput = findViewById(R.id.salaryInput);
+
+
+        //Main screen buttons Onclick listeners
+        siteRequest.setOnClickListener(this);
+        dailyReport.setOnClickListener(this);
+        salaryInput.setOnClickListener(this);
+
+    }
+
+    //respond to the button clicks on the home screen
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.siteRequest:
+               Intent siteRequestScreen = new Intent(this, SiteRequestActivity.class);
+               startActivity(siteRequestScreen);
+               break;
+
+            case R.id.dailyReport:
+                Intent dailyRptScreen = new Intent(this, DailyReportActivity.class);
+                startActivity(dailyRptScreen);
+                break;
+
+            case R.id.salaryInput:
+                Intent salaryInputScreen = new Intent(this, SalaryInputActivity.class);
+                startActivity(salaryInputScreen);
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ocube/siterequest/SiteRequestActivity.java
@@ -0,0 +1,14 @@
+package com.ocube.siterequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiteRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_site_request);
+    }
+}
